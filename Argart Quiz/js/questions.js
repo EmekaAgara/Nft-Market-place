d0378649925105e2fb856138964f601be750f6c0
EmekaAgara <emekaagara@gmail.com>
2022-04-09 13:52:32 +0100

    Argart Nft Marketplace

---
+++ Argart Quiz/js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of these is a paint made from pigments and plastic?",
+    answer: "Acrylic",
+    options: [
+      "Acrylic",
+      "Gesso",
+      "Acetone",
+      "Tempera"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Early photographers made their images on which of these materials?",
+    answer: "Glass",
+    options: [
+      "Glass",
+      "Stone",
+      "Paper",
+      "Plastic"
+    ]
+  },
+    {
+    numb: 3,
+    question: "To which artistic movement does paul Gauguins The Yellow Christ belong to ?",
+    answer: "Cloisonnism",
+    options: [
+      "Impressionism",
+      "Bauhuaus",
+      "cloisonnism",
+      "Favism"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does the venus of Brassemoupy represent?",
+    answer: "A woman's head",
+    options: [
+      "An angel",
+      "a human figure",
+      "A woman's head",
+      "an old man"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which architect founded the Bauhaus school of design?",
+    answer: "walter Groupius",
+    options: [
+      "walter Groupius",
+      "Frank Gehry",
+      "im Pel",
+      "Frank Lloyd"
+    ]
+  },
+];
---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+    {
+      price: 0.012,
+      from: 'realagara',
+      to: 'Azuna',
+      date: 'Just now',
+    },
+    {
+      price: 0.0034,
+      from: 'realagara',
+      to: 'Ozioma',
+      date: '45 minutes ago',
+    },
+    {
+      price: 0.0127,
+      from: 'Joe',
+      to: 'Chi Chi',
+      date: '55 minutes ago',
+    },
+    {
+      price: 0.003,
+      from: 'Felix',
+      to: 'Anonymous',
+      date: '3 days ago',
+    },
+  ]
